5ee88c22c69aa33bf9ecffc02cba9c238bb87a38
SethEPeters <speters32@academic.rrc.ca>
2026-07-08 23:33:09 -0500

    test prettier implementation

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,9 +1,9 @@
-import type { Meta, StoryFn, StoryObj } from "@storybook/react";
-import Button from "./Button";
+import type { Meta, StoryFn, StoryObj } from '@storybook/react';
+import Button from './Button';
 
 export default {
-  title: "Components/Button",
-  theme: "Components/Button",
+  title: 'Components/Button',
+  theme: 'Components/Button',
   component: Button,
 } as Meta<typeof Button>;
 
@@ -11,20 +11,20 @@ type Story = StoryObj<typeof Button>;
 
 export const ButtonDefault: Story = {
   args: {
-    label: "Default Theme",
+    label: 'Default Theme',
     state: true,
   },
   argTypes: {
-    background_colour: { control: "color" },
+    background_colour: { control: 'color' },
   },
 };
 
 export const ButtonDisabled: Story = {
   args: {
-    label: "Disabled Theme",
+    label: 'Disabled Theme',
     state: false,
   },
   argTypes: {
-    background_colour: { control: "color" },
+    background_colour: { control: 'color' },
   },
 };
