df0a17e44fc051162828ff51ffe71ebaa1c6bd02
SethEPeters <speters32@academic.rrc.ca>
2026-07-10 15:31:23 -0500

    use eslint on all files

--- src/components/Dropdown/Dropdown.tests.tsx
+++ src/components/Dropdown/Dropdown.tests.tsx
@@ -7,7 +7,7 @@ describe('Visible Dropdown Test', () => {
   it('check if the dropdown element is visible', () => {
     render(<Dropdown label="default" option1="an option" option2="another option" state={true} />);
 
-    const dropdown = screen.getByRole('option', {});
+    const dropdown = screen.getAllByRole('option', {});
 
     expect(dropdown).toBeVisible();
   });
